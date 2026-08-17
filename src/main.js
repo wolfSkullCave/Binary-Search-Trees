2c76dfdc24a227f01e532b6956ae4ed7a12e70e9
Kyle-Laptop <67805222+wolfSkullCave@users.noreply.github.com>
2026-08-17 16:30:36 +0200

    feat(main.js): Added a main file
    The main file allows for testing/viewing the with the prettyPrint
    module.

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { prettyPrint } from "./prettyPrint.js";
+import { Tree } from "./Tree.js";
+
+let testArr = [1, 0, 8, 3, 6, 1];
+
+const testTree = new Tree(testArr);
+
+console.log(prettyPrint(testTree.root));
