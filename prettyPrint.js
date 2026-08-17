b5bf994ab79b93d1eeb5f9feb60efd153362e83a
Kyle-Laptop <67805222+wolfSkullCave@users.noreply.github.com>
2026-08-17 15:21:36 +0200

    Initial commit

---
+++ prettyPrint.js
@@ -0,0 +1,11 @@
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null || node === undefined) {
+    return;
+  }
+
+  prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+};
+
+export { prettyPrint };
